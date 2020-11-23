0b3c72011dd54bddfc3b688e183cd6052984d8fd
Michael Abel <mabel@bitmuster.org>
2020-11-23 22:38:36 +0100

    generalise

--- new_protocol.py
+++ new_protocol.py
@@ -2,6 +2,7 @@
 import serial
 import time
 import sys
+import struct
 
 # install pyserial: https://pyserial.readthedocs.io/en/latest/pyserial.html
 
@@ -10,6 +11,12 @@ ser = serial.Serial('/dev/ttyUSB0', 57600, timeout=1.0)
 #ser.write(b"\xF6")
 
 HELLO = b"\xff\x09\x5a\xa5"
+ACK = b'\xff\x05\x01\x01'
+DOIT = b'\xff\x06\x01\x01'
+WHATEVER = b'\xff\x6e\x01\x02\x28\x02\x28'
+
+DIMENSIONS_T = b'\xff\x6e\x02'
+
 d = 0.1 # slow down to observe
 
 print("Read buffer emtty")
@@ -70,36 +77,37 @@ ser.write(intensity)
 #write
 #ff 6e 01 02 28 02 28 ff 6e 02 00 10 00 0a ff 06 01 01
 
-print('whatever 2')
+print('Write whatever')
 time.sleep(d)
-two = b'\xff\x6e\x01\x02\x28\x02\x28'
-ser.write(two)
+ser.write(WHATEVER)
 
-print('whatever 3')
+print('Write dimensions')
 time.sleep(d)
 
-three = b'\xff\x6e\x02\x00\x10\x00\x0a'
-ser.write(three)
+#width = b'\x00\x10'  # bit width
+width = height = struct.pack('>h', 16) # should be a multiple of 8
 
-print('4')
+#height = b'\x00\x0a' # 10 lines
+height = struct.pack('>h', 10)
+
+dim = DIMENSIONS_T + width + height
+ser.write(dim)
+
+print('Write DO IT')
 time.sleep(d)
-four = b'\xff\x06\x01\x01'
-ser.write(four)
+ser.write(DOIT)
 
-
-print("Read and hope")
+print("Read and hope for ACK")
 rep=ser.read(10);
 print(rep)
 
-#read
-#ff 05 01 01
-expok = b'\xff\x05\x01\x01'
+#read ack
 
 if len(rep) != 4:
     print('Ah')
     sys.exit(1)
 
-if rep == expok:
+if rep == ACK:
     pass
 else:
     print('Aah')
