c881bed46575b04f091ce948c2a1739f672e4414
Michael Abel <mabel@bitmuster.org>
2020-11-25 11:21:45 +0100

    Script to download the complete collection

---
+++ download_collection.py
@@ -0,0 +1,33 @@
+
+
+import os
+import sys
+
+name = 'collection_links.txt'
+
+f = open(name)
+
+outdir = 'Collection/'
+
+try: 
+    os.mkdir(outdir)
+except FileExistsError:
+    pass
+
+l = 0
+
+for i in f.readlines():
+    #print (i.split('"'))
+    if not str.isspace(i):
+        link = i.split('"')[1]
+        print (link , '  ->  ', end='')
+        n = link.split('/')
+        #print(n)
+        new_name = n[4] + '_' + n[5]
+        print(new_name)
+        cmd = 'curl ' + link + ' > ' + outdir + new_name
+        print(cmd)
+        os.system(cmd)
+        l += 1
+        #if l== 10:
+        #    sys.exit()
